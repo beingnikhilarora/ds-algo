8a32f820e48f78c1bbe27ec37418d7e5cd5567e9
Nikhil Arora <beingnikhilarora@gmail.com>
2019-04-19 14:15:50 +0530

    added knapsack python code

---
+++ python/0_1_knapsack.py
@@ -0,0 +1,17 @@
+def knapsack(wt, val, capacity):
+    m = [[0 for _ in range(capacity+1)] for _ in range(len(wt)+1)]
+    for i in range(len(wt)+1):
+        for j in range(capacity+1):
+            if j >= wt[i-1]:
+                m[i][j] = max(m[i-1][j-wt[i-1]]+val[i-1], m[i-1][j])
+            else:
+                m[i][j] = m[i-1][j]
+    return m[len(wt)][capacity]
+
+
+if __name__ == '__main__':
+    wt = [1, 2, 3]
+    val = [6, 10, 12]
+    capacity = 5
+    ans = knapsack(wt, val, capacity)
+    print(ans)
