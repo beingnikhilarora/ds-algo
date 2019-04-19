3074f8baa9b5fd3eebd56e965816d1a943632c10
Nikhil Arora <beingnikhilarora@gmail.com>
2019-04-19 14:47:40 +0530

    fixed a minor mistake in knapsack code

--- python/0_1_knapsack.py
+++ python/0_1_knapsack.py
@@ -1,13 +1,14 @@
 def knapsack(wt, val, capacity):
-    m = [[0 for _ in range(capacity+1)] for _ in range(len(wt)+1)]
-    for i in range(1,len(wt)+1):
-        for j in range(1,capacity+1):
+    K = [[0 for _ in range(capacity+1)] for _ in range(len(wt)+1)]
+
+    for i in range(1, len(wt)+1):
+        for j in range(1, capacity+1):
             if j >= wt[i-1]:
-                m[i][j] = max(m[i-1][j-wt[i-1]]+val[i-1], m[i-1][j])
+                K[i][j] = max(K[i-1][j-wt[i-1]]+val[i-1], K[i-1][j])
             else:
-                m[i][j] = m[i-1][j]
-    print(m)
-    return m[len(wt)][capacity]
+                K[i][j] = K[i-1][j]
+
+    return K[len(wt)][capacity]
 
 
 if __name__ == '__main__':
